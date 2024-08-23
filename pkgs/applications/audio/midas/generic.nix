020e06524acaf4f6250cdddacb30555e1106d990
daniel <danielgreenhome@gmail.com>
2024-08-23 17:52:36 +0100

    Modify Generic Midas Mixer derivation to allow for additional libraries and maintainers in some sub-packages

--- pkgs/applications/audio/midas/generic.nix
+++ pkgs/applications/audio/midas/generic.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, lib, libX11, libXext, alsa-lib, freetype, brand, type, version, homepage, url, sha256, ... }:
+{ stdenv, fetchurl, lib, libX11, libXext, alsa-lib, freetype, brand, type, version, homepage, url, sha256, extraMaintainters ? [], extraLibs ? [], ... }:
 stdenv.mkDerivation rec {
   pname = "${lib.toLower type}-edit";
   inherit version;
@@ -18,13 +18,14 @@ stdenv.mkDerivation rec {
   '';
   preFixup = let
     # we prepare our library path in the let clause to avoid it become part of the input of mkDerivation
-    libPath = lib.makeLibraryPath [
+    libPath = lib.makeLibraryPath ([
       libX11           # libX11.so.6
       libXext          # libXext.so.6
       alsa-lib          # libasound.so.2
       freetype         # libfreetype.so.6
       stdenv.cc.cc.lib # libstdc++.so.6
-    ];
+    ] ++ extraLibs      # Extra Libraries required by some packages built from this derivation but not all
+    );
   in ''
     patchelf \
       --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
@@ -38,6 +39,6 @@ stdenv.mkDerivation rec {
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
     platforms = platforms.linux;
-    maintainers = [ maintainers.magnetophon ];
+    maintainers = (with maintainters; [ magnetophon Green-D-683 ]) ++ extraMaintainters; ## Extra Maintainers for some subpackages
   };
 }
