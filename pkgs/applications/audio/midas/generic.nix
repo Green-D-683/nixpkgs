75a3f2177bc39aaea6a0f3f1aa3027c117a9df63
Daniel <danielgreenhome@gmail.com>
2025-03-07 03:44:30 +0000

    Working desktop icon for x-air-edit

--- pkgs/applications/audio/midas/generic.nix
+++ pkgs/applications/audio/midas/generic.nix
@@ -1,6 +1,26 @@
-{ stdenv, fetchurl, lib, libX11, libXext, alsa-lib, freetype, brand, type, version, homepage, url, sha256, extraMaintainters ? [], extraLibs ? [], ... }:
+{
+  stdenv,
+  fetchurl,
+  lib,
+  libX11,
+  libXext,
+  alsa-lib,
+  freetype,
+  brand,
+  type,
+  version,
+  homepage,
+  url,
+  sha256,
+  extraMaintainters ? [ ],
+  extraLibs ? [ ],
+
+  # The Icon of packages doesn't seem to be shipped in the packages consistently, hence the option to provide a path to the icon. Pass in "provided" if the icon is included as ${name}_icon.png in the $src
+  icon ? "",
+  ...
+}:
 let
-name = "${type}-Edit";
+  name = "${type}-Edit";
 in
 stdenv.mkDerivation rec {
   pname = "${lib.toLower name}";
@@ -9,45 +29,63 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     inherit url;
     inherit sha256;
-    postFetch = "ls";
   };
 
   sourceRoot = ".";
+
   dontBuild = true;
   dontStrip = true;
 
+  # install the application, as well as creating a desktop file for it. Ideally, this would also copy the icon from
   installPhase = ''
-    ls -a
     mkdir -p $out/bin
     cp ${name} $out/bin/${pname}
-    mkdir -p $out/share/icons
-    cp ${name}_icon.png $out/share/icons/${lib.toLower name}.png
+    ${
+      if (icon == "included") then # Copy the icon from $src to $out
+        ''
+          mkdir -p $out/share/icons
+          cp ${name}_icon.png $out/share/icons/${lib.toLower name}.png
+        ''
+      else
+        "" # No icon to copy
+    }
     mkdir -p $out/share/applications
-    cat <<INI > $out/share/applications/${name}.desktop
-        [Desktop Entry]
-        Terminal=false
-        Name=${name}
-        Exec=$out/bin/${pname}
-        Type=Application
-        Icon=${lib.toLower name}
-        INI
+    cat <<INI >> $out/share/applications/${name}.desktop
+    [Desktop Entry]
+    Terminal=false
+    Name=${name}
+    Exec=$out/bin/${pname}
+    Type=Application
+    Icon=${
+      if (icon == "included") then
+        (lib.toLower name) # If icon included in src then use that
+      else if (icon != "") then
+        icon # Else if icon defined use that
+      else
+        "xorg" # Otherwise default to xorg icon - it's at least an `X`
+    }
+    INI
   '';
-  preFixup = let
-    # we prepare our library path in the let clause to avoid it become part of the input of mkDerivation
-    libPath = lib.makeLibraryPath ([
-      libX11           # libX11.so.6
-      libXext          # libXext.so.6
-      alsa-lib          # libasound.so.2
-      freetype         # libfreetype.so.6
-      stdenv.cc.cc.lib # libstdc++.so.6
-    ] ++ extraLibs      # Extra Libraries required by some packages built from this derivation but not all
-    );
-  in ''
-    patchelf \
-      --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
-      --set-rpath "${libPath}" \
-      $out/bin/${pname}
-  '';
+  preFixup =
+    let
+      # we prepare our library path in the let clause to avoid it become part of the input of mkDerivation
+      libPath = lib.makeLibraryPath (
+        [
+          libX11 # libX11.so.6
+          libXext # libXext.so.6
+          alsa-lib # libasound.so.2
+          freetype # libfreetype.so.6
+          stdenv.cc.cc.lib # libstdc++.so.6
+        ]
+        ++ extraLibs # Extra Libraries required by some packages built from this derivation but not all
+      );
+    in
+    ''
+      patchelf \
+        --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+        --set-rpath "${libPath}" \
+        $out/bin/${pname}
+    '';
 
   meta = with lib; {
     inherit homepage;
@@ -55,6 +93,11 @@ stdenv.mkDerivation rec {
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license = licenses.unfree;
     platforms = platforms.linux;
-    maintainers = (with maintainters; [ magnetophon Green-D-683 ]) ++ extraMaintainters; ## Extra Maintainers for some subpackages
+    maintainers =
+      (with maintainters; [
+        magnetophon
+        Green-D-683
+      ])
+      ++ extraMaintainters; # # Extra Maintainers for some subpackages
   };
 }
--- pkgs/applications/audio/midas/x-air-edit.nix
+++ pkgs/applications/audio/midas/x-air-edit.nix
@@ -1,16 +1,33 @@
-{ callPackage, lib, curl, libGL, ... } @ args:
+{
+  callPackage,
+  curl,
+  libGL,
 
-callPackage ./generic.nix (args // rec {
-  brand = "Behringer";
-  type = "X-AIR";
-  version = "1.8.1";
-  url = "https://mediadl.musictribe.com/download/software/behringer/XAIR/${version}/${type}-Edit_LINUX_${version}.tar.gz";
-  sha256 = "sha256-vFy3/iAsGs1IlBSYAX5zTghbPtBfFCUtqVFxfMsFCGY=";
-  homepage = "https://www.behringer.com/series.html?category=R-BEHRINGER-XAIRSERIES";
+  # Since Behringer/Midas no longer ship the icon in their tarballs (present in v1.8, not v1.8.1), the icon must be manually downloaded, using the icon from Flathub purely as a convenience - this should be a consistent url and hence be a reliable fetch
+  icon ? (
+    builtins.fetchurl {
+      url = "https://dl.flathub.org/media/com/behringer/XAirEdit/ae8f33db1bb473961cc67ae58b82d81f/icons/128x128/com.behringer.XAirEdit.png";
+      sha256 = "sha256:1acd2l71a25y11v9f2ld6wcl3rarcibga1lsp6fzmfc8wq2js0fi";
+    }
+  ),
+  ...
+}@args:
 
-  # In addition to the libraries used by all midas mixer applications, X-AIR-Edit requires the following:
-  extraLibs = [
-    curl      # libcurl.so.4
-    libGL     # libGL.so.1
-  ];
-})
+callPackage ./generic.nix (
+  args
+  // rec {
+    brand = "Behringer";
+    type = "X-AIR";
+    version = "1.8.1";
+    url = "https://mediadl.musictribe.com/download/software/behringer/XAIR/${version}/${type}-Edit_LINUX_${version}.tar.gz";
+    sha256 = "sha256-vFy3/iAsGs1IlBSYAX5zTghbPtBfFCUtqVFxfMsFCGY=";
+    homepage = "https://www.behringer.com/series.html?category=R-BEHRINGER-XAIRSERIES";
+
+    # In addition to the libraries used by all midas mixer applications, X-AIR-Edit requires the following:
+    extraLibs = [
+      curl # libcurl.so.4
+      libGL # libGL.so.1
+    ];
+    inherit icon;
+  }
+)
